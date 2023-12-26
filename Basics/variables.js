700b883e49eb1bfe375fc4ef5d14a9bf97b6dbc4
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-12-26 19:44:35 +0000

    container config done

---
+++ Basics/variables.js
@@ -0,0 +1,15 @@
+const accId = 23344;
+let accName = "zaidKhan"
+let accEmail = "zaidKhan@google.com"
+
+location = "jaipur"
+
+// accId = 34; Not allowed.
+
+accName = "AsifAhmad"
+accEmail = "AsifAhmad00134@gmail.com"
+location = "Bangaluru"
+
+console.log(accName);
+
+console.table([accName, accEmail, accId, location])
