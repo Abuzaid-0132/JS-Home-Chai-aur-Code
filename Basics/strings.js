935f7aa9184e17e50f9f1c2f619f3739e3ddaa41
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-12-28 11:22:02 +0000

    Strings in JS

---
+++ Basics/strings.js
@@ -0,0 +1,21 @@
+const name = "Zaid Khan"
+const repoCount = 100
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} & my repo count is ${repoCount} `);
+
+const gameName = new String("Abuzaid-khan-com")
+console.log(gameName.length);
+
+const newString = gameName.slice(0, 3);
+console.log(newString);
+
+const StringOne = "   Hi_there     "
+console.log(StringOne);
+console.log(StringOne.trim())
+
+const url = "https://Abuzaid.com%20ZaidKhan"
+console.log(url.replace('%20', '--'));
+
+console.log(gameName.split('-'));
