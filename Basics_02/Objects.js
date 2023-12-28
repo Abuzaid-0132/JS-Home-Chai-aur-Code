e21d4859417ea37e18d2e89265ce9cb83d09cbb8
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-12-28 15:23:57 +0000

    Object de-structure

--- Basics_02/Objects.js
+++ Basics_02/Objects.js
@@ -6,7 +6,7 @@
 const mySym = Symbol("key1")
 
 
-const JsUser = {
+ const JsUser = {
     name: "Zaid Khan",
     "full name": "Mr. Zaid Khan",
     [mySym]: "myNewkey1",
@@ -36,3 +36,54 @@ JsUser.greetingTwo = function(){
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+
+ // const tinderUser = new Object(); Singleton Object Created.
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+
+  console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "Zaid",
+            lastname: "Khan"
+        }
+    }
+}
+
+ console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b", 3: "c"}
+const obj2 = {3: "d", 4:"e", 5: "f"}
+
+// const obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+const course = {
+    coursename: "JS Home",
+    price: 999,
+    courseInstructor: "Zaid Khan"
+}
+
+// course.courseInstructor
+
+const {courseInstructor} = course
+console.log(courseInstructor);
+
