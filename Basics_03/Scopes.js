993b1f1f34214bb3015ae61e3d472487b8520b37
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-12-29 07:13:11 +0000

    Scope in JS

---
+++ Basics_03/Scopes.js
@@ -0,0 +1,14 @@
+function One() {
+    const username = "Zaid Khan"
+
+    function Two() {
+        const website = "YouTube"
+
+        console.log(username);
+    }
+
+    //console.log(website);
+    Two();
+}
+
+One();
