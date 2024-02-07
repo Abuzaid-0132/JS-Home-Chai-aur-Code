ca2834bad3894c225c589fc20e260830cc26fbde
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2024-02-07 18:09:28 +0000

    1st Project Color Scheme Switcher

---
+++ Color_Scheme_Switcher_Project/script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+  // console.log(button);
+  button.addEventListener('click', (event) => {
+    // console.log(event);
+    // console.log(event.target);
+    if (event.target.id === 'grey') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'white') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'yellow') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'blue') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'purple') {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
+});
